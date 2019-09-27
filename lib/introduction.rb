c80e92e5b500db07e01d1d35927fed1f6e6a545f
Bahay Gulle Bilgi <bahayg@gmail.com>
2019-09-27 20:47:28 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -33,66 +33,6 @@ end
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 # def introduction(name)
 #  puts "Hi, my name is #{name}."
 #end
